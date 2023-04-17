f98f6a7ea0aa24cf8ee130db9ed5f16c9b3daa94
Salathiel Vieira <49722370+salatvieira@users.noreply.github.com>
2023-04-17 13:52:10 +0000

    Adiciona página inicial

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+function Home() {
+
+    return <h1>Bruna, você é uma pessoa muito especial para mim. Gostaria de estar perto para cuidar de você. Me mande um emoji no Telegram :)</h1>
+
+}
+
+export default Home;
