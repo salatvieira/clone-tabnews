570b371ccc2b5454eff2405051ecc5fd160ab96f
Salathiel Vieira <49722370+salatvieira@users.noreply.github.com>
2023-05-03 19:22:13 +0000

    Nova frase

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function Home() {
 
-    return <h1>Bruna, você é uma pessoa muito especial para mim. Gostaria de estar perto para cuidar de você. Me mande um emoji no Telegram :)</h1>
+    return <h1>Valor...</h1>
 
 }
 
