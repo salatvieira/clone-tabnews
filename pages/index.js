f8a48d24f4d1684f0a9f2ede22376fc984efa199
Salathiel Vieira <49722370+salatvieira@users.noreply.github.com>
2023-07-24 22:43:20 +0000

    Modificação teste

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,5 @@
 function Home() {
-  return (
-    <h1>
-      Se você quiser alguém para ser só seu, é só não se esquecer, estarei aqui.
-    </h1>
-  );
+  return <h1>Se você quiser alguém para ser só seu, é só não se esquecer.</h1>;
 }
 
 export default Home;
